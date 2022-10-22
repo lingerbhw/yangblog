86c9e8a879bd320d2f2eb8b9b51b914561960745
lingerbhw <3501737127@qq.com>
2022-10-23 03:04:05 +0800

    Site updated: 2022-10-23 03:03:55

---
+++ js/sw_local.js
@@ -0,0 +1,2 @@
+pjax.site_handleResponse=pjax.handleResponse,pjax.handleResponse=function(e,n,s,p){Object.defineProperty(n,"responseURL",{value:s}),pjax.site_handleResponse(e,n,s,p)};
+//# sourceMappingURL=../maps/js/sw_local.js.map
